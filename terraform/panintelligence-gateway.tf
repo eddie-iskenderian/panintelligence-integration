6aa012164d943d43143edd1eefc736512a973edf
Eddie Iskenderian <eddie@slyp.com.au>
2023-03-11 17:39:01 +1100

    Added ECS target groups

--- terraform/panintelligence-gateway.tf
+++ terraform/panintelligence-gateway.tf
@@ -1,4 +1,26 @@
 
+resource "aws_apigatewayv2_domain_name" "pi_poc_domain" {
+  domain_name = "dashboards.data.team-slyp.com.au"
+
+  domain_name_configuration {
+    certificate_arn = "arn:aws:acm:ap-southeast-2:824763547294:certificate/98f4d37e-35b0-4c18-94bc-b80c249abacb"
+    endpoint_type   = "REGIONAL"
+    security_policy = "TLS_1_2"
+  }
+}
+
+resource "aws_route53_record" "pi_dns_record" {
+  name    = aws_apigatewayv2_domain_name.pi_poc_domain.domain_name
+  type    = "A"
+  zone_id = "Z00505302C72GZTJUJ2J1"
+
+  alias {
+    name                   = aws_apigatewayv2_domain_name.pi_poc_domain.domain_name_configuration[0].target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.pi_poc_domain.domain_name_configuration[0].hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
 resource "aws_lb" "pi_alb" {
   name               = "pi-alb"
   internal           = true
@@ -15,47 +37,70 @@ resource "aws_lb" "pi_alb" {
   }
 }
 
-resource "aws_lb_target_group" "pi_alb_tg" {
-  port        = 443
+resource "aws_lb_target_group" "pi_alb_tomcat_tg" {
+  port        = 8224
   protocol    = "HTTPS"
-  target_type = "ip"
   vpc_id      = "vpc-0668d30a9cf84fc3e"
 }
 
-#########
-resource "tls_private_key" "example" {
-  algorithm = "RSA"
+resource "aws_lb_target_group_attachment" "pi_alb_tomcat_tg_att" {
+  target_group_arn = aws_lb_target_group.pi_alb_tomcat_tg.arn
+  target_id        = "i-0545232137aa0a9de"
+  port             = 8224
 }
 
-resource "tls_self_signed_cert" "example" {
-  private_key_pem = tls_private_key.example.private_key_pem
-
-  subject {
-    common_name  = "slyp.com.au"
-    organization = "Slyp Pty Ltd"
+resource "aws_lb_listener" "pi_alb_tomcat_listener" {
+  load_balancer_arn = aws_lb.pi_alb.arn
+  port              = "8224"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:ap-southeast-2:824763547294:certificate/98f4d37e-35b0-4c18-94bc-b80c249abacb"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.pi_alb_tomcat_tg.arn
   }
-
-  validity_period_hours = 12
-
-  allowed_uses = [
-    "key_encipherment",
-    "digital_signature",
-    "server_auth",
-  ]
 }
 
-resource "aws_acm_certificate" "cert" {
-  private_key      = tls_private_key.example.private_key_pem
-  certificate_body = tls_self_signed_cert.example.cert_pem
+resource "aws_lb_target_group" "pi_alb_http_tg" {
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = "vpc-0668d30a9cf84fc3e"
 }
-########
+
+resource "aws_lb_target_group_attachment" "pi_alb_http_tg_att" {
+  target_group_arn = aws_lb_target_group.pi_alb_http_tg.arn
+  target_id        = "i-0545232137aa0a9de"
+  port             = 80
+}
+
+resource "aws_lb_listener" "pi_alb_http_listener" {
+  load_balancer_arn = aws_lb.pi_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.pi_alb_http_tg.arn
+  }
+}
+
+resource "aws_lb_target_group" "pi_alb_tg" {
+  port        = 443
+  protocol    = "HTTPS"
+  vpc_id      = "vpc-0668d30a9cf84fc3e"
+}
+
+resource "aws_lb_target_group_attachment" "pi_alb_tg_att" {
+  target_group_arn = aws_lb_target_group.pi_alb_tg.arn
+  target_id        = "i-0545232137aa0a9de"
+  port             = 443
+}
 
 resource "aws_lb_listener" "pi_alb_listener" {
   load_balancer_arn = aws_lb.pi_alb.arn
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = aws_acm_certificate.cert.arn
+  certificate_arn   = "arn:aws:acm:ap-southeast-2:824763547294:certificate/98f4d37e-35b0-4c18-94bc-b80c249abacb"
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.pi_alb_tg.arn
@@ -94,4 +139,10 @@ resource "aws_apigatewayv2_stage" "default_stage" {
   api_id      = aws_apigatewayv2_api.pi_api.id
   name        = "$default"
   auto_deploy = true
+}
+
+resource "aws_apigatewayv2_api_mapping" "friendly_domain_mapping" {
+  api_id      = aws_apigatewayv2_api.pi_api.id
+  domain_name = aws_apigatewayv2_domain_name.pi_poc_domain.id
+  stage       = aws_apigatewayv2_stage.default_stage.id
 }
